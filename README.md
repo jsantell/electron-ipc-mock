2cfbe679f21f8b71ddccd9583a233a0123cc45b4
Jordan Santell <jordan@jsantell.com>
2016-04-20 15:31:41 -0700

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# electron-ipc-mock
-Mock channels for Electron's IPC communication
+electron-ipc-mock
+=======
+
+[![Build Status](http://img.shields.io/travis/jsantell/electron-ipc-mock.svg?style=flat-square)](https://travis-ci.org/jsantell/electron-ipc-mock)
+[![Build Status](http://img.shields.io/npm/v/electron-ipc-mock.svg?style=flat-square)](https://www.npmjs.org/package/electron-ipc-mock)
+
+Mock Communication for Electron's IPC
+
+An API mimicking [Electron's](http://electron.atom.io/docs/latest/api/ipc-renderer/) [ipcMain](http://electron.atom.io/docs/latest/api/ipc-main/) and [ipcRenderer](http://electron.atom.io/docs/latest/api/ipc-renderer/).
+
+## Install
+
+```
+npm install electron-ipc-mock
+```
+
+## Testing
+
+```
+npm test
+```
+
+## License
+
+MIT License, Copyright (c) 2016 Jordan Santell
---
+++ test/test.communication.js
@@ -0,0 +1,33 @@
+var chai = require('chai');
+var expect = chai.expect;
+var IpcMock = require('../');
+
+describe('electron-ipc-mock', function () {
+  beforeEach(function () {
+    var mock = IpcMock();
+    this.ipcRenderer = mock.ipcRenderer;
+    this.ipcMain = mock.ipcMain;
+  });
+
+  it('can communicate asynchronously', function (done) {
+    var ipcRenderer = this.ipcRenderer;
+    var ipcMain = this.ipcMain;
+
+    // Bind listener after sending request to ensure asyncness
+    ipcRenderer.send('test-event', 1, null, 'hello');
+
+    ipcMain.on('test-event', function (e, arg1, arg2, arg3) {
+      expect(arg1).to.be.equal(1);
+      expect(arg2).to.be.equal(null);
+      expect(arg3).to.be.equal('hello');
+
+      // Bind listener after sending response to ensure asyncness
+      e.sender.send('test-response', 'hello', 'world');
+      ipcRenderer.on('test-response', function (arg1, arg2) {
+        expect(arg1).to.be.equal('hello');
+        expect(arg2).to.be.equal('world');
+        done();
+      });
+    });
+  });
+});
